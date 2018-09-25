eed07e458edf7ed1d028df13bd5d81eabe52e33c
Toby Ferguson <toby@cloudera.com>
2018-09-25 09:32:26 -0700

    change to chronyd bootstrap
    Fixed incorrect reference to chrony config file

--- director/scripts/bootstrap-chronyd.sh
+++ director/scripts/bootstrap-chronyd.sh
@@ -8,6 +8,6 @@ exec > ~/bootstrap-chronyd.$(date +%Y%m%d%H%M%S).log 2>&1
 systemctl stop chronyd
 systemctl stop ntpd
 systemctl disable ntpd.service
-sed -i.$(date +%Y%m%d%H%M%S).bak -e "s/server 0.centos.pool.ntp.org iburst/server 169.254.169.123 prefer iburst/" /etc/chronyd.conf
+sed -i.$(date +%Y%m%d%H%M%S).bak -e "s/server 0.centos.pool.ntp.org iburst/server 169.254.169.123 prefer iburst/" /etc/chrony.conf
 systemctl start chronyd
 
